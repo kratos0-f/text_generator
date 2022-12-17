bf06505eb4492bae734d54d12916bc2e921800be
kratos0-f <protopovichevgeny0@gmail.com>
2022-12-18 00:36:41 +0300

    Сделан скелет бота

---
+++ src/keyboards.py
@@ -0,0 +1,11 @@
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, \
+    KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
+
+button_help = KeyboardButton("/Help")
+button_generate_text = KeyboardButton("/Generate_text")
+button_show_stats = KeyboardButton("/Statistic")
+
+greet_kb = ReplyKeyboardMarkup(resize_keyboard=True)
+greet_kb.add(button_help)
+greet_kb.add(button_generate_text)
+greet_kb.add(button_show_stats)
--- src/main.py
+++ src/main.py
@@ -1,15 +1,29 @@
 import TOKEN
 from aiogram import Bot, Dispatcher, executor
 from aiogram.types import Message
+from aiogram.dispatcher.filters import Text, Command
+
+import keyboards
 
 TOKEN_API = TOKEN.TOKEN
 
-bot = Bot(token=TOKEN_API)
+bot = Bot(token=TOKEN_API, parse_mode="HTML")
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def start(message: Message):
-    await message.answer("Hello!")
+    await message.reply("Hello!", reply_markup=keyboards.greet_kb)
+
+@dp.message_handler(commands=["Generate_text"])
+async def text_generator(message: Message, command: Command):
+    if command.args:
+        await message.answer(f"Вы хотите сгенерировать текст из <b>{command.args}</b> слов")
+    else:
+        await message.answer("После команды \Generate_text введите количество слов.")
+
+@dp.message_handler(commands=["Help"])
+async def help(message: Message):
+    await message.answer("...")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
